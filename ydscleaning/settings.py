0e63eee470ae13f059f349005c02e5e728b07ac3
Erik Lopez <eriklopezalfaro@outlook.com>
2022-03-30 22:39:43 -0700

    local settings

--- ydscleaning/settings.py
+++ ydscleaning/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-b))9v9hrjpru)n==acx2y+mj)dd)4l2mpg^%9k!+s+%oz$au3i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['eriklopez.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.ydscommercialcleaning.com']
 
 
 # Application definition
