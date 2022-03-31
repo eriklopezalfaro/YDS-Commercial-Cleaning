00d3b3dde18d6700d54717dbf36a618411053388
Erik Lopez <eriklopezalfaro@outlook.com>
2022-03-30 20:30:17 -0700

    First commit

---
+++ services/models.py
@@ -0,0 +1,16 @@
+from distutils.command.upload import upload
+from pydoc import describe
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    image = models.ImageField(upload_to='services/image/')
+
+    def __str__(self):
+        return self.title
+
+
+
